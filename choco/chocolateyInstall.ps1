b84fb56df896ed4a94402c8cf6d0dc768d75358e
Gary Ewan Park <gep13@gep13.co.uk>
2015-12-10 20:49:13 +0000

    (GH-57) Updating to reflect chocolatey.org package which has been pushed already
    https://chocolatey.org/packages/openlivewriter

--- choco/chocolateyInstall.ps1
+++ choco/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
-$packageName = 'openLiveWriter.install'
-$fileType = 'exe'
+$packageName= 'openlivewriter'
+$fileType = 'EXE'
 $silentArgs = '--silent'
 $scriptPath =  $(Split-Path $MyInvocation.MyCommand.Path)
-$fileFullPath = Join-Path $scriptPath 'Setup.exe'
+$fileFullPath = Join-Path $scriptPath 'OpenLiveWriterSetup.exe'
 
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $fileFullPath
+
+Write-Output "The install log is at `"$env:localappdata\SquirrelTemp\SquirrelSetup.log`""
