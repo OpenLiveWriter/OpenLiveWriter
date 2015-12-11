968a088ed09f9ddeefb45d796307e5095df21afd
Josh Soref <apache@soref.com>
2015-12-11 19:45:46 +0000

    spelling: needed

--- src/managed/OpenLiveWriter.Interop/Com/IExtractImage.cs
+++ src/managed/OpenLiveWriter.Interop/Com/IExtractImage.cs
@@ -31,7 +31,7 @@ namespace OpenLiveWriter.Interop.Com
         public const uint ASYNC = 0x0001;      // ask the extractor if it supports ASYNC extract (free threaded)
         public const uint CACHE = 0x0002;      // returned from the extractor if it does NOT cache the thumbnail
         public const uint ASPECT = 0x0004;      // passed to the extractor to beg it to render to the aspect ratio of the supplied rect
-        public const uint OFFLINE = 0x0008;      // if the extractor shouldn't hit the net to get any content neede for the rendering
+        public const uint OFFLINE = 0x0008;      // if the extractor shouldn't hit the net to get any content needed for the rendering
         public const uint GLEAM = 0x0010;      // does the image have a gleam ? this will be returned if it does
         public const uint SCREEN = 0x0020;      // render as if for the screen  (this is exclusive with IEIFLAG_ASPECT )
         public const uint ORIGSIZE = 0x0040;      // render to the approx size passed, but crop if necessary
