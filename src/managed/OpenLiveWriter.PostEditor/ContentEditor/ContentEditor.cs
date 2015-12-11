60cbca1d4a401ea1513a113aecdf9e1ba3938faa
Josh Soref <apache@soref.com>
2015-12-11 19:04:28 +0000

    spelling: bizarro

--- src/managed/OpenLiveWriter.PostEditor/ContentEditor/ContentEditor.cs
+++ src/managed/OpenLiveWriter.PostEditor/ContentEditor/ContentEditor.cs
@@ -2251,7 +2251,7 @@ namespace OpenLiveWriter.PostEditor
                 {
                     using (new WaitCursor())
                     {
-                        // fixup bizzaro table selections
+                        // fixup bizarro table selections
                         IHtmlEditorComponentContext editorContext = _currentEditor as IHtmlEditorComponentContext;
                         if (editorContext != null)
                         {
