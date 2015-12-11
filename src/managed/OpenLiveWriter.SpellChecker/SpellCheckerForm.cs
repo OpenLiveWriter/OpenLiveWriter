2ea3546d517722e24fce3d4162f728ab69100ef2
Josh Soref <apache@soref.com>
2015-12-11 19:16:05 +0000

    spelling: dictionary

--- src/managed/OpenLiveWriter.SpellChecker/SpellCheckerForm.cs
+++ src/managed/OpenLiveWriter.SpellChecker/SpellCheckerForm.cs
@@ -441,7 +441,7 @@ namespace OpenLiveWriter.SpellChecker
         /// <param name="e">event args</param>
         private void buttonAdd_Click(object sender, EventArgs e)
         {
-            // add this word to the user dictioanry
+            // add this word to the user dictionary
             spellingChecker.AddToUserDictionary(labelWord.Text);
 
             // continue spell checking
