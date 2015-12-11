bca19bb089a40d38ccc72fe165ca2584eb1a2cef
Josh Soref <apache@soref.com>
2015-12-11 17:11:19 +0000

    spelling: only

--- src/managed/OpenLiveWriter.CoreServices/WebRequest/UrlHelper.cs
+++ src/managed/OpenLiveWriter.CoreServices/WebRequest/UrlHelper.cs
@@ -590,7 +590,7 @@ namespace OpenLiveWriter.CoreServices
 
         /// <summary>
         /// Returns the base path from a full Url.
-        /// This basically strips the filename from a URL leaving oonly its directory.
+        /// This basically strips the filename from a URL leaving only its directory.
         /// Warning: if a URL directory is passed in without a trailing slash, the directory
         /// name will be considered a filename, and will be stripped.
         /// </summary>
