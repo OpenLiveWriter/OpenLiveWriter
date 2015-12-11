c917355091bb49f2bb1c843bd252e2e7b81ee74b
Josh Soref <apache@soref.com>
2015-12-11 20:23:04 +0000

    spelling: scheme

--- src/managed/OpenLiveWriter.BlogClient/Clients/SharePointClient.cs
+++ src/managed/OpenLiveWriter.BlogClient/Clients/SharePointClient.cs
@@ -366,16 +366,16 @@ namespace OpenLiveWriter.BlogClient.Clients
             internal AuthenticationScheme GetAuthenticationScheme()
             {
                 string authVal = _blogCredentials.GetCustomValue(AUTH_SCHEME);
-                AuthenticationScheme authSceme = AuthenticationScheme.Http;
+                AuthenticationScheme authScheme = AuthenticationScheme.Http;
                 if (authVal != null && authVal.Length > 0)
                 {
                     try
                     {
-                        authSceme = (AuthenticationScheme)AuthenticationScheme.Parse(typeof(AuthenticationScheme), authVal);
+                        authScheme = (AuthenticationScheme)AuthenticationScheme.Parse(typeof(AuthenticationScheme), authVal);
                     }
                     catch (Exception) { }//unknown authscheme detected
                 }
-                return authSceme;
+                return authScheme;
             }
         }
     }
