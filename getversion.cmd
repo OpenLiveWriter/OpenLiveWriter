b9384db1e33a56c0e25dc4af411e47e98299fcde
Ashhar Hasan <hashhar_dev@outlook.com>
2016-12-15 03:43:49 +0530

    Update build scripts for newer versions of NuGet.exe

--- getversion.cmd
+++ getversion.cmd
@@ -2,7 +2,7 @@
 
 SET /P version=<"%~dp0version.txt"
 
-FOR /F "tokens=1-4 delims=." %%V in ("%version%") DO (
-    SET "dottedVersion=%%V.%%W.%%X.%%Y"
-    SET "rcVersion=%%V,%%W,%%X,%%Y"
+FOR /F "tokens=1-3 delims=." %%V in ("%version%") DO (
+    SET "dottedVersion=%%V.%%W.%%X"
+    SET "rcVersion=%%V,%%W,%%X"
 )
