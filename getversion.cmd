834194cdb3323bbd0d7c0486f52c2df08db35224
Jon Galloway <jongalloway@gmail.com>
2017-03-13 18:49:37 -0700

    Updated getversion.cmd to work with NuGet normalized versions.

--- getversion.cmd
+++ getversion.cmd
@@ -3,6 +3,7 @@
 SET /P version=<"%~dp0version.txt"
 
 FOR /F "tokens=1-4 delims=." %%V in ("%version%") DO (
-    SET "dottedVersion=%%V.%%W.%%X.%%Y"
+    ::NuGet now normalizes versions, so need to remove build number if 0
+    IF %%Y EQU 0 (SET "dottedVersion=%%V.%%W.%%X") ELSE (SET "dottedVersion=%%V.%%W.%%X.%%Y")
     SET "rcVersion=%%V,%%W,%%X,%%Y"
 )
