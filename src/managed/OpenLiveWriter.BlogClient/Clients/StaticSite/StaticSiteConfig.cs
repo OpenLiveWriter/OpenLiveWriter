b7b0c6b5770c9df262e4516f0ba169c57b3eca40
Nick Vella <nick@nxk.io>
2019-07-03 16:05:42 +1000

    ssg: StaticSiteConfig: create

---
+++ src/managed/OpenLiveWriter.BlogClient/Clients/StaticSite/StaticSiteConfig.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLiveWriter.BlogClient.Clients
+{
+    public class StaticSiteConfig
+    {
+        // The credential keys where the configuration is stored.
+        private const string CONFIG_POSTS_PATH = "SSGPostsPath";
+        private const string CONFIG_PAGES_PATH = "SSGPagesPath";
+        private const string CONFIG_BUILD_COMMAND = "SSGBuildCommand";
+        private const string CONFIG_PUBLISH_COMMAND = "SSGPublishCommand";
+
+        /// <summary>
+        /// The full path to the local static site 'project' directory
+        /// </summary>
+        public string LocalSitePath { get; set; }
+
+        /// <summary>
+        /// Path to Posts directory, relative to LocalSitePath
+        /// </summary>
+        public string PostsPath { get; set; }
+
+        /// <summary>
+        /// Path to Pages directory, relative to LocalSitePath
+        /// </summary>
+        public string PagesPath { get; set; }
+
+        /// <summary>
+        /// Build command, executed by system command interpreter with LocalSitePath working directory
+        /// </summary>
+        public string BuildCommand { get; set; }
+
+        /// <summary>
+        /// Publish command, executed by system command interpreter with LocalSitePath working directory
+        /// </summary>
+        public string PublishCommand { get; set; }
+
+        /// <summary>
+        /// Load site configuration from blog credentials
+        /// </summary>
+        /// <param name="creds">An IBlogCredentialsAccessor</param>
+        public void LoadFromCredentials(IBlogCredentialsAccessor creds)
+        {
+            LocalSitePath  = creds.Username;
+            PostsPath      = creds.GetCustomValue(CONFIG_POSTS_PATH);
+            PagesPath      = creds.GetCustomValue(CONFIG_PAGES_PATH);
+            BuildCommand   = creds.GetCustomValue(CONFIG_BUILD_COMMAND);
+            PublishCommand = creds.GetCustomValue(CONFIG_PUBLISH_COMMAND);
+        }
+
+        /// <summary>
+        /// Saves site configuration to blog credentials
+        /// </summary>
+        public void SaveToCredentials(IBlogCredentialsAccessor creds)
+        {
+            // Set username to Local Site Path
+            creds.Username = LocalSitePath;
+            creds.SetCustomValue(CONFIG_POSTS_PATH,      PostsPath);
+            creds.SetCustomValue(CONFIG_PAGES_PATH,      PagesPath);
+            creds.SetCustomValue(CONFIG_BUILD_COMMAND,   BuildCommand);
+            creds.SetCustomValue(CONFIG_PUBLISH_COMMAND, PublishCommand);
+        }
+
+        public void SaveToCredentials(IBlogCredentials blogCredentials)
+            => SaveToCredentials(new BlogCredentialsAccessor("", blogCredentials));
+
+        /// <summary>
+        /// Create a new StaticSiteConfig instance and load site configuration from blog credentials
+        /// </summary>
+        /// <param name="blogCredentials">An IBlogCredentialsAccessor</param>
+        public static StaticSiteConfig LoadConfigFromCredentials(IBlogCredentialsAccessor blogCredentials)
+        {
+            var config = new StaticSiteConfig();
+            config.LoadFromCredentials(blogCredentials);
+            return config;
+        }
+
+        public static StaticSiteConfig LoadConfigFromCredentials(IBlogCredentials blogCredentials)
+            => LoadConfigFromCredentials(new BlogCredentialsAccessor("", blogCredentials));
+    }
+}
