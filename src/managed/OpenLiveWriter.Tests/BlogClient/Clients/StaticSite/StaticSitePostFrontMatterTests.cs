a3d48dbb27f47bd01622cf4427c0edbd91f40a8b
Nick Vella <nick@nxk.io>
2019-07-12 00:00:11 +1000

    tests: ssg: tests for StaticSitePostFrontMatter#Deserialize

---
+++ src/managed/OpenLiveWriter.Tests/BlogClient/Clients/StaticSite/StaticSitePostFrontMatterTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using OpenLiveWriter.BlogClient.Clients.StaticSite;
+
+namespace OpenLiveWriter.Tests.BlogClient.Clients.StaticSite
+{
+    [TestFixture]
+    class StaticSitePostFrontMatterTests
+    {
+        [Test]
+        public void Deserialize_Basic()
+        {
+            // Expected
+            var expected = new StaticSitePostFrontMatter()
+            {
+                Title = "Test title",
+                Date = "2019-01-01 00:00:00",
+                Layout = "post",
+                Tags = new string[] { "programming", ".net" }
+            };
+
+            // Act
+            var fm = new StaticSitePostFrontMatter();
+            fm.Deserialize(@"title: Test title
+date: 2019-01-01 00:00:00
+layout: post
+tags:
+  - programming
+  - .net");
+
+            // Assert
+            Assert.AreEqual(fm.Title, expected.Title);
+            Assert.AreEqual(fm.Date, expected.Date);
+            Assert.AreEqual(fm.Layout, expected.Layout);
+            Assert.IsTrue(Enumerable.SequenceEqual(fm.Tags, expected.Tags));
+        }
+    }
+}
