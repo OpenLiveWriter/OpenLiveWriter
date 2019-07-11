fc1503163ab1b31a03f55ff7d35bd43d138acf03
Nick Vella <nick@nxk.io>
2019-07-12 00:02:48 +1000

    tests: ssg: missing keys test for StaticSitePostFrontMatter#Deserialize

--- src/managed/OpenLiveWriter.Tests/BlogClient/Clients/StaticSite/StaticSitePostFrontMatterTests.cs
+++ src/managed/OpenLiveWriter.Tests/BlogClient/Clients/StaticSite/StaticSitePostFrontMatterTests.cs
@@ -34,5 +34,29 @@ tags:
             Assert.AreEqual(fm.Layout, expected.Layout);
             Assert.IsTrue(Enumerable.SequenceEqual(fm.Tags, expected.Tags));
         }
+
+        [Test]
+        public void Deserialize_MissingKeys()
+        {
+            // Expected
+            var expected = new StaticSitePostFrontMatter()
+            {
+                Title = "Test title",
+                Date = "2019-01-01 00:00:00",
+                Tags = new string[] {}
+            };
+
+            // Act
+            var fm = new StaticSitePostFrontMatter();
+            fm.Deserialize(@"title: Test title
+date: 2019-01-01 00:00:00");
+
+            // Assert
+            Assert.AreEqual(fm.Title, expected.Title);
+            Assert.AreEqual(fm.Date, expected.Date);
+            Assert.AreEqual(fm.Layout, expected.Layout);
+            Assert.IsTrue(Enumerable.SequenceEqual(fm.Tags, expected.Tags));
+        }
+
     }
 }
