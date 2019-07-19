409e99110194ab9fba6fb74249fa48c0865c0c6c
Nick Vella <nick@nxk.io>
2019-07-19 17:05:04 +1000

    ssg: StaticSitePage impl

---
+++ src/managed/OpenLiveWriter.BlogClient/Clients/StaticSite/StaticSitePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+using OpenLiveWriter.CoreServices;
+using OpenLiveWriter.Extensibility.BlogClient;
+
+namespace OpenLiveWriter.BlogClient.Clients.StaticSite
+{
+    public class StaticSitePage : StaticSitePost
+    {
+        public StaticSitePage(StaticSiteConfig config) : base(config)
+        {
+        }
+
+        public StaticSitePage(StaticSiteConfig config, BlogPost blogPost) : base(config, blogPost)
+        {
+        }
+
+
+    }
+}
